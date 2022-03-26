b9d735e08d80218648e4a41f59e9192af9dc2605
gabrielprdg <gabriel.pr07@hotmail.com>
2022-03-25 23:52:12 -0300

    test: test env2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -32,7 +32,7 @@ export function Header({isLoginPage, isAdminPage}: HeaderProps){
           <Link href="/">
             <div className={styles.title}>
               <h2 className={styles.use}>
-                Use
+                {process.env.PUBLIC_KEY_MERCADO_PAGO}
               </h2>
               <div className={styles.fsl}>
                 FashionStoreLook
