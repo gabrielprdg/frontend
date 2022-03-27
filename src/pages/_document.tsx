ce775cc0f6826c82711700fc17336066db96f9b8
gabrielprdg <gabriel.pr07@hotmail.com>
2022-03-26 22:21:00 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                  
+                  <link rel="shortcut icon" href="usefashion.ico" type="image/x-icon" />
                   <link rel="preconnect" href="https://fonts.googleapis.com"/>
                   <link rel="preconnect" href="https://fonts.googleapis.com"/>
                   <link rel="preconnect" href="https://fonts.gstatic.com" data-crossorigin/>
