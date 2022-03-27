7fec1a929d92feae37b9e40969d6db2ab7213a21
gabrielprdg <gabriel.pr07@hotmail.com>
2022-03-26 22:14:40 -0300

    feat: changing name of titleProd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,7 @@ export default function Home({ prod, latestProducts, categories }: HomeProps) {
       
       <div className={styles.topic}> 
         <span className={styles.dash}></span>
-          <p className={styles.titleProd}>f{process.env.PUBLIC_KEY_MERCADO_PAGO} </p>
+          <p className={styles.titleProd}> CONHEÇA UM POUCO DA NOSSA LOJA </p>
         <span className={styles.dash}></span>
       </div>
 
